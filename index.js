0e8ad5a616e0dcca618348e788c0ca090d1d2472
tanaphon.k@pc <tanaphon.k@outlook.com>
2023-06-03 18:31:17 +0700

    update webhook

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const port = process.env.PORT || 3000;
 const googleApiKey = process.env.GOOGLE_API_KEY;
 const channelSecret = process.env.LINE_CHANNEL_SECRET; // Line Channel secret string
 
+const asyncMiddleware = fn =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+  };
+
 /**
  * Prints the names and majors of students in a sample spreadsheet:
  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
@@ -37,45 +43,43 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.post('/webhook', async (req, res) => {
-    const rcvSignature = req.header("x-line-signature");
-    if (rcvSignature != null && rcvSignature != '' && rcvSignature != ' ') {
-        const body = req.body;
-        const bodyString = JSON.stringify(body);
-        const signature = createHmac("SHA256", channelSecret).update(bodyString).digest("base64");
-        if (rcvSignature == signature) {
-            console.log(body);
-
-            if (body.events && body.events.length > 0 && typeof body.events === 'object') {
-              const event = body.events[0];
-              if (event && event.type == 'message') {
-                const message = event.message;
-                if (message && message.type == 'text') {
-                  const text = message.text;
-                  const replyToken = message.replyToken;
-                  console.log(`Receive message: ${text}`);
-                  const majors = await getMajors();
-                  const results = majors.filter(x => x.major.toLowerCase() == text.toLowerCase());
-                  let resText = results.length > 0 ? results.join(",") : 'Not found!';
-                  const url = 'https://api.line.me/v2/bot/message/reply';
-                  await axios.post(url, {
-                    replyToken: replyToken,
-                    messages: [ { type: 'text', text: resText } ]
-                  }, {
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'Authorization': `Bearer ${channelSecret}`
-                    }
-                  });
-                }
+app.post('/webhook', asyncMiddleware(async (req, res) => {
+  const rcvSignature = req.header("x-line-signature");
+  if (rcvSignature != null && rcvSignature != '' && rcvSignature != ' ') {
+      const body = req.body;
+      const bodyString = JSON.stringify(body);
+      const signature = createHmac("SHA256", channelSecret).update(bodyString).digest("base64");
+      if (rcvSignature == signature) {
+          if (body.events && body.events.length > 0 && typeof body.events === 'object') {
+            const event = body.events[0];
+            if (event && event.type == 'message') {
+              const message = event.message;
+              if (message && message.type == 'text') {
+                const text = message.text;
+                const replyToken = message.replyToken;
+                console.log(`Receive message: ${text}`);
+                const majors = await getMajors();
+                const results = majors.filter(x => x.major.toLowerCase() == text.toLowerCase());
+                let resText = results.length > 0 ? results.join(",") : 'Not found!';
+                const url = 'https://api.line.me/v2/bot/message/reply';
+                await axios.post(url, {
+                  replyToken: replyToken,
+                  messages: [ { type: 'text', text: resText } ]
+                }, {
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${channelSecret}`
+                  }
+                });
               }
             }
+          }
 
-            res.status(200).send();
-        }
-    }
-    res.status(401).send();
-})
+          res.status(200).send();
+      }
+  }
+  res.status(401).send();
+}))
   
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
