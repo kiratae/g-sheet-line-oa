ac40d1cf0679ba0d85fdb0bc7a52ae567dbab6ef
tanaphon.k@pc <tanaphon.k@outlook.com>
2023-06-03 18:25:51 +0700

    update webhook

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createHmac } from "crypto";
 import { google } from 'googleapis';
+import axios from 'axios';
 import 'dotenv/config'
 
 const app = express();
@@ -14,7 +15,8 @@ const channelSecret = process.env.LINE_CHANNEL_SECRET; // Line Channel secret st
  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-async function listMajors() {
+async function getMajors() {
+  const list = [];
   const sheets = google.sheets({version: 'v4', auth: googleApiKey});
   const res = await sheets.spreadsheets.values.get({
     spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
@@ -23,32 +25,52 @@ async function listMajors() {
   const rows = res.data.values;
   if (!rows || rows.length === 0) {
     console.log('No data found.');
-    return;
+    return list;
   }
-  console.log('Name, Major:');
   rows.forEach((row) => {
-    // Print columns A and E, which correspond to indices 0 and 4.
-    console.log(`${row[0]}, ${row[4]}`);
+    list.push({ name: row[0], major: row[4] });
   });
+  return list;
 }
-
-
-
-// const googleClient = await authorize();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-    listMajors();
 })
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', async (req, res) => {
     const rcvSignature = req.header("x-line-signature");
-    if(rcvSignature != null && rcvSignature != '' && rcvSignature != ' '){
+    if (rcvSignature != null && rcvSignature != '' && rcvSignature != ' ') {
         const body = req.body;
         const bodyString = JSON.stringify(body);
         const signature = createHmac("SHA256", channelSecret).update(bodyString).digest("base64");
         if (rcvSignature == signature) {
             console.log(body);
+
+            if (body.events && body.events.length > 0 && typeof body.events === 'object') {
+              const event = body.events[0];
+              if (event && event.type == 'message') {
+                const message = event.message;
+                if (message && message.type == 'text') {
+                  const text = message.text;
+                  const replyToken = message.replyToken;
+                  console.log(`Receive message: ${text}`);
+                  const majors = await getMajors();
+                  const results = majors.filter(x => x.major.toLowerCase() == text.toLowerCase());
+                  let resText = results.length > 0 ? results.join(",") : 'Not found!';
+                  const url = 'https://api.line.me/v2/bot/message/reply';
+                  await axios.post(url, {
+                    replyToken: replyToken,
+                    messages: [ { type: 'text', text: resText } ]
+                  }, {
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': `Bearer ${channelSecret}`
+                    }
+                  });
+                }
+              }
+            }
+
             res.status(200).send();
         }
     }
